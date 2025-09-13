bed4a4cb9d26480ed5b72a4d17d77f52cb6cd882
divyanshpatwari <divyanshpatwari36@gmail.com>
2025-09-13 19:37:28 +0530

    equalto_pipe

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ spark = SparkSession.builder.getOrCreate()
 print("====================== pyspark started ==================")
 print()
 
-df = spark.read.csv("equalto.csv",sep="=").withColumnRenamed("_c0","columns").withColumnRenamed("_c1","details")
+df = spark.read.csv("equalto.csv",sep="=").withColumnRenamed("_c0","columns").withColumnRenamed("_c1","details") # READ CSV FILE 'equalto.csv'
 df.show()
 
-df1 = (df.withColumn("details",split(col("details"),"\\|"))
-       .selectExpr("columns","posexplode(details)as(pos,value)")
+df1 = (df.withColumn("details",split(col("details"),"\\|")) # NOTE: To split data with delimiter '|(pipe)' you need to give an escape character which is '\\(two backslashes)'
+       .selectExpr("columns","posexplode(details)as(pos,value)") # This line will explode the list and assign index to every element that was split
        )
 df1.show()
 
-fdf = (df1.groupBy("pos").pivot("columns").agg(first("value")).drop("pos")
+fdf = (df1.groupBy("pos").pivot("columns").agg(first("value")).drop("pos") # Group by index nos. (pos) and pivot will convert the selected column values into actual columns
        .selectExpr("Worker_ID","Name","Department","Salary")
-       .na.replace("",None)
+       .na.replace("",None) # Replace empty values with NULL
        )
 fdf.show()
 
